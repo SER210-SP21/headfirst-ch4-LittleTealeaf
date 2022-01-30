c0b59ba042c63bd3a50d7ec313d2fa031addf0ca
Thomas Kwashnak <shintaomonk@gmail.com>
2022-01-29 19:33:54 -0500

    Added Layout

--- app/src/main/java/com/example/demochapter04/StopwatchActivity.java
+++ app/src/main/java/com/example/demochapter04/StopwatchActivity.java
@@ -3,6 +3,7 @@ package com.example.demochapter04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class StopwatchActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class StopwatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+    }
+
+    public void onClickStart(View view) {
+
+    }
+
+    public void onClickStop(View view) {
+
+    }
+
+    public void onClickReset(View view) {
+
     }
 }
